f818cfcaf76b6187c1a62559454a64439c4308f1
Hessam <hessam.ashari@gmail.com>
2018-08-25 13:30:05 +0430

    change welcome

--- install.sh
+++ install.sh
@@ -18,13 +18,13 @@ blue="$(printf '\033[0;34m')"         # blue
 white="$(printf '\033[0;37m')"        # white
 
 echo -e "\n"
-echo "+-------------------------------------------------------------------------------------+"
-echo "|          Video usefull tools powered by ffmpeg and find command                     |"
-echo "|          Author: Hessam Ashari                                                      |"
-echo "|          Git repository: https://github.com/hessamashari/video-tool                 |"
-echo "|          Platform: GNU/Linux                                                        |"
-echo "|          Version: 0.4                                                               |"
-echo "+-------------------------------------------------------------------------------------+"
+echo "+----------------------------------------------------------------------------------------+"
+echo "|          Video usefull commands powered by ffmpeg and find command                     |"
+echo "|          Author: Hessam Ashari                                                         |"
+echo "|          Git repository: https://github.com/hessamashari/video-tool                    |"
+echo "|          Platform: GNU/Linux                                                           |"
+echo "|          Version: 0.4                                                                  |"
+echo "+----------------------------------------------------------------------------------------+"
 echo -e "\n"
 
 # ----------\Install dependencies\---------
