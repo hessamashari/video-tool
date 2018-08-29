ed0a3c6c875363db0fb5f96316a9af766b82249d
Hessam <hessam.ashari@gmail.com>
2018-08-29 11:45:53 +0430

    add update and uninstall file in src/main/

---
+++ src/main/update.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+    # This is the part of Video_Tool project on https://github.com/hessamashari/video-tool
+
+normal="$(printf '\033[0m')"          # text mode
+# Colors
+red="$(printf '\033[0;31m')"  		  # red
+green="$(printf '\033[0;32m')"        # green
+orange="$(printf '\033[0;33m')"       # orange
+blue="$(printf '\033[0;34m')"         # blue
+white="$(printf '\033[0;37m')"        # white
+
+# ----------\Update function\----------
+function update() {
+	clear
+
+	# Check place of repo for existing files
+	if [[ -d /tmp/video-tool/ ]]; then
+		sudo rm -rf /tmp/video-tool/
+	fi
+
+	# Check access of /tmp
+	if cd /tmp/ &> /dev/null; then
+		git clone https://github.com/hessamashari/video-tool.git /tmp/video-tool
+		sleep 2
+		echo -e "\n"
+		# Check access of /tmp/video-tool
+		if cd /tmp/video-tool/ &> /dev/null; then
+			# Replace older program with newer
+			sudo cp /tmp/video-tool/video-tool /usr/bin/video-tool
+			sudo chmod 755 /usr/bin/video-tool
+			echo -e "\n"
+			# Check the correctness of the operation
+			if [[ "$?" == "0" ]]; then
+				echo -e " ${green}Video_Tool updated succesfully! ${normal}"
+			else
+				echo -e " ${red}Video_Tool doesn't update succesfully! ${normal}"
+				exit 1
+			fi
+		else
+			echo -e "\n"
+			echo -e "${red}Cannot cd to directory ${orange}'/tmp/video-tool'${normal}"
+			exit 1
+		fi
+	else
+		echo -e "\n"
+		echo -e "${red}Cannot cd to directory ${orange}'/tmp'${normal}"
+		exit 1
+	fi
+
+	# Delet cloned repository
+	sudo rm -rf /tmp/video-tool
+}
