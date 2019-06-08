d94dddb380d2bc05d21d0ffbfc832ced3c3cdb1b
Hessam Ahsari <hessam.ashari@gmail.com>
2019-06-08 20:27:16 +0430

    Add convert into GIF option

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ cat <<- EOF
 |          Git repository: https://github.com/hessamashari/video-tool           |
 |          Platform: GNU/Linux                                                  |
 |          LICENSE: GNU GPLv3                                                   |
-|          Version: 0.7                                                         |
+|          Version: 0.8                                                         |
 +-------------------------------------------------------------------------------+
 EOF
 echo -e "\n"
