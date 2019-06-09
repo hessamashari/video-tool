63b52e56738407aabf82489b3d391f2e5c4e835b
Hessam Ahsari <hessam.ashari@gmail.com>
2019-06-09 20:30:27 +0430

    atom beautify was badgit add *

--- src/removeAudio.sh
+++ src/removeAudio.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# This file is part of Video_Tool.
-#
-# Video_Tool is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Video_Tool is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+    # This file is part of Video_Tool.
+    #
+    # Video_Tool is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+    #
+    # Video_Tool is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
 
 normal="$(printf '\033[0m')"          # text mode
 # Colors
